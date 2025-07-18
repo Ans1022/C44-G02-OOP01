bcc57dfc7dbdf71c63f1c4e6ab3dc5b96b0f2709
Anas Madkour <anas.mohamed0976@gmail.com>
2025-07-18 21:24:13 +0300

    OOP Session01

---
+++ Assignment/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment;
+using System.Xml.Linq;
+
+namespace Assignment
+{
+    public class Employee
+    {
+        public int ID; 
+        public string Name;
+
+        private char gender;
+        public char Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == 'M' || value == 'F')
+                    gender = value;
+                else
+                    throw new ArgumentException("Gender must be 'M' or 'F'");
+            }
+        }
+
+        public SecurityLevel Security { get; set; }
+        public decimal Salary { get; set; }
+        public HiringDate HireDate { get; set; }
+
+        public Employee (int id, string name, char gender, SecurityLevel security, decimal salary, HiringDate hireDate)
+        {
+            ID = id;
+            Name = name;
+            Gender = gender;
+            Security = security;
+            Salary = salary;
+            HireDate = hireDate;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}, Name: {Name}, Gender: {Gender}, Security: {Security}, Salary: {string.Format("{0:C}", Salary)}, Hire Date: {HireDate}";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,117 @@
-﻿namespace Assignment
+﻿using Assignment;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace Assignment
 {
+    //struct point
+    //{
+    //    public double x;
+    //    public double y;
+    //}
+
+    //struct person
+    //{
+
+    //    public string name;
+    //    public int age;
+    //}
+
+    public enum SecurityLevel
+    {
+        Guest,
+        Developer,
+        Secretary,
+        DBA
+    }
+
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 [part 1]
+            //point p1, p2;
+
+            //Console.WriteLine("Enter X for poin 1 : ");
+            //p1.x = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("Enter Y for poin 1 : ");
+            //p1.y = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("Enter X for poin 2 : ");
+            //p2.x = Convert.ToDouble(Console.ReadLine());
+
+            // Console.WriteLine("Enter Y for poin 2 : ");
+            //p2.y = Convert.ToDouble(Console.ReadLine());
+
+            //double dx = p1.x - p2.x; 
+            //double dy = p1.y - p2.y; 
+
+            //double distance = dx * dx + dy * dy;
+            //Console.WriteLine("Distance = " + distance);
+
+
+
+            #endregion
+
+            #region Q2 [part 1]
+            //person p1, p2, p3;
+
+            //Console.WriteLine("Enter name of person 1 :");
+            //p1.name = Console.ReadLine() + "0";
+
+            //Console.WriteLine("Enter Age of person 1 :");
+            //p1.age =Convert.ToInt32 (Console.ReadLine());
+
+
+            //Console.WriteLine("Enter name of person 2 :");
+            //p2.name = Console.ReadLine() + "0";
+
+            //Console.WriteLine("Enter Age of person 2 :");
+            //p2.age =Convert.ToInt32 (Console.ReadLine());
+
+
+            //Console.WriteLine("Enter name of person 3 :");
+            //p3.name = Console.ReadLine() + "0";
+
+            //Console.WriteLine("Enter Age of person 3 :");
+            //p3.age =Convert.ToInt32 (Console.ReadLine());
+
+            // person oldest = p1;
+
+            //if (p2.age > oldest.age) 
+            //      oldest = p2;
+
+            //if (p3.age > oldest.age) 
+            //      oldest = p3;
+
+            //Console.WriteLine("Oldest person: " + oldest.name);
+            //Console.WriteLine("Age: " + oldest.age);
+            #endregion
+
+
+            #region Q3 [part 2]
+            Employee[] EmpArr = new Employee[3];
+
+            EmpArr[0] = new Employee(1, "Anas", 'M', SecurityLevel.DBA, 12000, new HiringDate(10, 5, 2020));
+
+            EmpArr[1] = new Employee(2, "Mohamed", 'F', SecurityLevel.Guest, 7000, new HiringDate(15, 7, 2021));
+
+            EmpArr[2] = new Employee(3, "Omar", 'M', SecurityLevel.Developer, 9000, new HiringDate(1, 1, 2022));
+
+            foreach (var emp in EmpArr)
+            {
+                Console.WriteLine(emp);
+            }
+            #endregion
+
+
         }
     }
 }
+
+    
+    
+
